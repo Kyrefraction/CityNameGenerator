b9e2fcb204de308e65b6f22e2d40cbe7931686d2
Vincenzo <v.scialpi@hotmail.co.uk>
2018-08-13 12:46:34 +0100

    Add files via upload
    C# version with Graphical User Interface

---
+++ program/Form1.Designer.cs
@@ -0,0 +1,179 @@
+﻿namespace cityNameGenerator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.italianButton = new System.Windows.Forms.RadioButton();
+            this.germanButton = new System.Windows.Forms.RadioButton();
+            this.menButton = new System.Windows.Forms.RadioButton();
+            this.evilButton = new System.Windows.Forms.RadioButton();
+            this.elvenButton = new System.Windows.Forms.RadioButton();
+            this.label1 = new System.Windows.Forms.Label();
+            this.genButton = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.displayBox = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // italianButton
+            // 
+            this.italianButton.AutoSize = true;
+            this.italianButton.Font = new System.Drawing.Font("Caviar Dreams", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.italianButton.Location = new System.Drawing.Point(59, 104);
+            this.italianButton.Name = "italianButton";
+            this.italianButton.Size = new System.Drawing.Size(88, 29);
+            this.italianButton.TabIndex = 0;
+            this.italianButton.TabStop = true;
+            this.italianButton.Text = "Italian";
+            this.italianButton.UseVisualStyleBackColor = true;
+            // 
+            // germanButton
+            // 
+            this.germanButton.AutoSize = true;
+            this.germanButton.Font = new System.Drawing.Font("Caviar Dreams", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.germanButton.Location = new System.Drawing.Point(250, 104);
+            this.germanButton.Name = "germanButton";
+            this.germanButton.Size = new System.Drawing.Size(104, 29);
+            this.germanButton.TabIndex = 1;
+            this.germanButton.TabStop = true;
+            this.germanButton.Text = "German";
+            this.germanButton.UseVisualStyleBackColor = true;
+            // 
+            // menButton
+            // 
+            this.menButton.AutoSize = true;
+            this.menButton.Font = new System.Drawing.Font("Caviar Dreams", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.menButton.Location = new System.Drawing.Point(388, 104);
+            this.menButton.Name = "menButton";
+            this.menButton.Size = new System.Drawing.Size(154, 29);
+            this.menButton.TabIndex = 2;
+            this.menButton.TabStop = true;
+            this.menButton.Text = "Realm of men";
+            this.menButton.UseVisualStyleBackColor = true;
+            // 
+            // evilButton
+            // 
+            this.evilButton.AutoSize = true;
+            this.evilButton.Font = new System.Drawing.Font("Caviar Dreams", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.evilButton.Location = new System.Drawing.Point(59, 154);
+            this.evilButton.Name = "evilButton";
+            this.evilButton.Size = new System.Drawing.Size(60, 29);
+            this.evilButton.TabIndex = 3;
+            this.evilButton.TabStop = true;
+            this.evilButton.Text = "Evil";
+            this.evilButton.UseVisualStyleBackColor = true;
+            // 
+            // elvenButton
+            // 
+            this.elvenButton.AutoSize = true;
+            this.elvenButton.Font = new System.Drawing.Font("Caviar Dreams", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.elvenButton.Location = new System.Drawing.Point(250, 154);
+            this.elvenButton.Name = "elvenButton";
+            this.elvenButton.Size = new System.Drawing.Size(79, 29);
+            this.elvenButton.TabIndex = 4;
+            this.elvenButton.TabStop = true;
+            this.elvenButton.Text = "Elven";
+            this.elvenButton.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Caviar Dreams", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(49, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(493, 56);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "City Name Generator";
+            // 
+            // genButton
+            // 
+            this.genButton.Font = new System.Drawing.Font("Caviar Dreams", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.genButton.Location = new System.Drawing.Point(180, 401);
+            this.genButton.Name = "genButton";
+            this.genButton.Size = new System.Drawing.Size(220, 88);
+            this.genButton.TabIndex = 6;
+            this.genButton.Text = "Generate";
+            this.genButton.UseVisualStyleBackColor = true;
+            this.genButton.Click += new System.EventHandler(this.genButton_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Caviar Dreams", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(263, 206);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(66, 22);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Names";
+            // 
+            // displayBox
+            // 
+            this.displayBox.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.displayBox.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.displayBox.Font = new System.Drawing.Font("Caviar Dreams", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.displayBox.Location = new System.Drawing.Point(79, 231);
+            this.displayBox.Multiline = true;
+            this.displayBox.Name = "displayBox";
+            this.displayBox.Size = new System.Drawing.Size(437, 164);
+            this.displayBox.TabIndex = 8;
+            this.displayBox.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(576, 531);
+            this.Controls.Add(this.displayBox);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.genButton);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.elvenButton);
+            this.Controls.Add(this.evilButton);
+            this.Controls.Add(this.menButton);
+            this.Controls.Add(this.germanButton);
+            this.Controls.Add(this.italianButton);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RadioButton italianButton;
+        private System.Windows.Forms.RadioButton germanButton;
+        private System.Windows.Forms.RadioButton menButton;
+        private System.Windows.Forms.RadioButton evilButton;
+        private System.Windows.Forms.RadioButton elvenButton;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button genButton;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox displayBox;
+    }
+}
+
---
+++ program/Form1.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace cityNameGenerator
+{
+    public partial class Form1 : Form
+    {
+        int numberOfNames = 5;
+        int namesPrinted = 0;
+        // Fantasy Elven Style City Name Generator 5
+        static String[] prefixes5 = { "Riv", "Loth", "Tir", "Aqu", "Gond", "Fir", "For", "Mirk", "Mith", "Galad", "Gal", "Cel", "Sir", "Luc", "Val", "Glor" };
+        static String[] suffixes5 = { "endell", "ariel", "en Haven", "olin", "alonde", "ion", "ian", "en Woods", "an Riverlands" };
+
+        // Fantasy Evil Style City Name Generator 4
+
+        static String[] prefixes4 = { "Mor", "Izen", "Khan", "Dagor", "Lich", "Kro", "Wor", "Gorgo", "Iron", "Dagen", "Dre", "Darken" };
+        static String[] suffixes4 = { "dor", "gard", "karak", "land", "moth", "gen", "roth", "forge", "fort", "den" };
+
+        // British Fantasy Style City Name Generator 3
+        static String[] prefixes3 = { "Norther", "Wessen", "Talken", "Tolken", "Larken", "Litten", "Litter", "Britter", "Souther", "Wester", "Easter", "Farken", "Falken", "Tarr", "Marr", "Ports", "Brad", "Lei", "Herr" };
+        static String[] suffixes3 = { "land", "glade", "den", "feld", "brough", "bridge", "chester", "mouth", "water", "ford", "ham" };
+
+        // German Style City Name Generator 2
+        static String[] prefixes2 = { "Ber", "Ham", "Mu", "Frank", "Dres", "Nurem", "Dussel", "Stutt", "Bre", "Lu", "Dort", "Augs", "Saar", "Ro", "Wurtz", "Wurtem", "Olden", "Klagen" };
+        static String[] suffixes2 = { "lin", "burg", "berg", "furt", "dorf", "gart", "beck", "brucken", "dam" };
+
+
+        // Italian Style City Name Generator 1
+        static String[] prefixes1 = { "Tor", "Ver", "Pad", "Mess", "Gen", "Pr", "Sal", "Ferr", "Lat", "Camp", "Berg", "Sirac", "Tr", "Nov", "Anc", "Lec", "Piac", "Liv", "Nav","R","L","Tar" };
+        static String[] suffixes1 = { "ini", "ati", "oli", "osa", "ata", "anto", "ona", "onte", "ano", "ecchio", "ere", "ino", "ello", "ana", "agna", "asco", "ate", "ero", "azzo", "ezzo", "aro", "ano", "ano","oma" };
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void genButton_Click(object sender, EventArgs e)
+        {
+            resetDisplayBox();
+            if (italianButton.Checked == true)
+            {
+                populateDisplayBoxPrelim(numberOfNames, prefixes1, suffixes1);
+            }
+            else if(germanButton.Checked == true)
+            {
+                populateDisplayBoxPrelim(numberOfNames, prefixes2, suffixes2);
+            }
+            else if(menButton.Checked == true)
+            {
+                populateDisplayBoxPrelim(numberOfNames, prefixes3, suffixes3);
+            } else if(evilButton.Checked == true)
+            {
+                populateDisplayBoxPrelim(numberOfNames, prefixes4, suffixes4);
+            } else if(elvenButton.Checked == true)
+            {
+                populateDisplayBoxPrelim(numberOfNames, prefixes5, suffixes5);
+            }
+        }
+
+        public void resetDisplayBox()
+        {
+            namesPrinted = 0;
+            displayBox.Text = "";
+        }
+        public void populateDisplayBoxPrelim(int amount, String[] prefixArray, String[] suffixArray)
+        {
+            Random r = new Random();
+            for (int i = 0; i < amount; i++)
+            {
+                populateDisplayBox(prefixArray, suffixArray, r);
+            }
+        }
+        public void populateDisplayBox(String[] prefixArray, String[] suffixArray, Random r)
+        {
+            String name = getRandom(prefixArray,r);
+            name = name + "" + getRandom(suffixArray,r);
+            displayBox.Text += name;
+            namesPrinted += 1;
+            if (namesPrinted == numberOfNames)
+            {
+                displayBox.Text += ".";
+            }
+            else
+            {
+                displayBox.Text += ", ";
+            }
+        }
+
+        public static String getRandom(String[] array, Random r)
+        {
+            var randomIndex = r.Next(0, array.Length);
+            return array[randomIndex];
+        }
+    }
+}
